ca6a3272763880dca896858c949c1bbd3ea7d50d
ValentinKalinin1989 <k-valentin-1989@yandex.ru>
2020-01-28 23:19:36 +0300

    Добавление связанных объектов в таблицу.

---
+++ src/main/java/com/luv2code/demo/CreateDemo.java
@@ -0,0 +1,40 @@
+package com.luv2code.demo;
+
+import com.luv2code.demo_entity.Instructor;
+import com.luv2code.demo_entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class CreateDemo {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .buildSessionFactory();
+
+        Session session = factory.getCurrentSession();
+
+        try {
+            Instructor tempInstructor =
+                    new Instructor("Chad", "Darby", "darby@luv2code.come");
+            InstructorDetail tempInstructorDetail = new InstructorDetail(
+                    "http://www.luv2code.com",
+                    "Luv 2 code!!!");
+            tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+            session.beginTransaction();
+
+            System.out.println("Saving instructor: " + tempInstructor);
+            session.save(tempInstructor);
+
+            session.getTransaction().commit();
+        } finally {
+            session.close();
+        }
+
+    }
+
+}
+
